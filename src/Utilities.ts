b2b399faeaf47f7aad372916dcc7f79adc7ff744
Jim Farrell <farrelljames558@gmail.com>
2020-11-15 11:27:44 +0100

    Remove debug log

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -16,7 +16,6 @@ export function npo2(x: number): number {
 export function TemplateClass(compiler: (...args: any[]) => string) {
   return (...args: any[]) => {
     const compiled = compiler.apply(null, args);
-    console.log(compiled);
     const wrapper = new Function(`return(${compiled})`);
     return wrapper();
   };
